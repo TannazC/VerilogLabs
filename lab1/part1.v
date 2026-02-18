4aff274c994eb838985dffff5dcda20d5c66b673
Tannaz <TannazHChowdhury@gmail.com>
2026-02-17 16:25:02 -0800

    Enhance part1.v with comments and module details
    Added header comments with lab details and license information.

--- lab1/part1.v
+++ lab1/part1.v
@@ -1,6 +1,15 @@
-// simple module that connects the SW switches to the LEDR lights
-module part1 (SW, LEDR);
-  input  [9:0] SW;    // toggle switches
-  output [9:0] LEDR;  // LEDs
-  assign LEDR = SW;
+// Lab 1 - Part 1
+// Switch to LED Direct Mapping
+// Tannaz C.
+// 2026-02-17
+// MIT License
+// DE1-SoC (Cyclone V 5CSEMA5F31C6)
+
+module part1 (
+    input  [9:0] SW,     // 10 slide switches
+    output [9:0] LEDR    // 10 red LEDs
+);
+
+    assign LEDR = SW;    // each LED reflects its corresponding switch
+
 endmodule
